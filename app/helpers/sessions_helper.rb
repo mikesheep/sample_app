ad000a983307af31fadb61b5bb7b29ca239841f5
mikesheep <mike0229hituji@gmail.com>
2025-04-25 07:33:00 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,6 +31,11 @@ module SessionsHelper
     end
   end
 
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -50,5 +55,9 @@ module SessionsHelper
     @current_user = nil   # 安全のため
   end
   
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
 
