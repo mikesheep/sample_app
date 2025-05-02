7d0edcb205c7a344994e03c12c654e0bc611da73
mikesheep <mike0229hituji@gmail.com>
2025-05-02 02:43:58 +0000

    OK

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
     :authentication => :plain,
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
